49d3823f369eb62deaa9c8564f19ebcad454b21b
codebendr-product-designer <codebendr@gmail.com>
2020-09-13 15:10:50 +0000

    added string extension

---
+++ Sources/Relative/Extensions/String.swift
@@ -0,0 +1,17 @@
+//
+//  String.swift
+//  Relative
+//
+//  Created by codebendr on 13/09/2020.
+//
+
+import Foundation
+
+public extension String {
+
+    static let empty = ""
+    static let success = "00"
+    static let code = "code"
+    static let data = "data"
+    
+}
