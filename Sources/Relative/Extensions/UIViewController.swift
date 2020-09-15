8ea629e07d9a268fd64b21faf82543341eda161c
codebendr-product-designer <codebendr@gmail.com>
2020-09-15 21:45:53 +0000

    added uiview extension

---
+++ Sources/Relative/Extensions/UIViewController.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewController.swift
+//  Relative
+//
+//  Created by codebendr on 15/09/2020.
+//
+
+import Foundation
+
+public @nonobjc extension UIViewController {
+    
+    func add(_ child: UIViewController, frame: CGRect? = nil) {
+        addChild(child)
+        
+        if let frame = frame {
+            child.view.frame = frame
+        }
+        
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+    
+    func remove() {
+        willMove(toParent: nil)
+        view.removeFromSuperview()
+        removeFromParent()
+    }
+}
