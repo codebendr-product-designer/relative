469e585c8dfecb2e0b4f35752a55f71e194f735d
codebendr-product-designer <codebendr@gmail.com>
2020-09-15 16:54:36 +0000

    added some default sizing

---
+++ Sources/Relative/Extensions/CGFloat.swift
@@ -0,0 +1,14 @@
+//
+//  CGSize.swift
+//  Relative
+//
+//  Created by codebendr on 15/09/2020.
+//
+
+import Foundation
+
+extension CGFloat {
+    static let small: CGFloat = 8
+    static let large: CGFloat = 16
+    static let extraLarge: CGFloat = 32
+}
