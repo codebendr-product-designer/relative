dfff0762e847ac3a38b1b0a85142746c91d7024f
codebendr-product-designer <codebendr@gmail.comm>
2020-10-27 12:55:22 +0000

    added position view extensions

--- Sources/Relative/Extensions/UIView.swift
+++ Sources/Relative/Extensions/UIView.swift
@@ -44,15 +44,85 @@ public extension UIView {
     }
     
     
-    /// helper function for setting translatesAutoresizingMaskIntoConstraints to false
-    /// - Parameter constraints: list of view constraints
-    func layout(using constraints: [NSLayoutConstraint]) {
-          translatesAutoresizingMaskIntoConstraints = false
-          NSLayoutConstraint.activate(constraints)
-      }
     
     class func fromNib<T: UIView>() -> T {
         return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
+    }
+    
+}
+
+// MARK: Layout
+
+public extension UIView {
+    
+    /// helper function for setting translatesAutoresizingMaskIntoConstraints to false
+    /// - Parameter constraints: list of view constraints
+    func layout(using constraints: [NSLayoutConstraint]) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    func fitted(in view: UIView, edgeInsets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: view.topAnchor, constant: edgeInsets.top),
+            bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: edgeInsets.bottom),
+            leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: edgeInsets.left),
+            trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: edgeInsets.right),
+        ])
+    }
+    
+    func x(in view: UIView, _ constant: CGFloat = 0) -> NSLayoutConstraint {
+        return centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: constant)
+    }
+    
+    func y(in view: UIView, _ constant: CGFloat = 0) -> NSLayoutConstraint {
+        return centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant)
+    }
+    
+    func left(in view: UIView, _ constant: CGFloat = 0) -> NSLayoutConstraint {
+        return leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: constant)
+    }
+    
+    func right(in view: UIView, _ constant: CGFloat = 0) -> NSLayoutConstraint {
+        return trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: constant)
+    }
+    
+    func top(in view: UIView, _ constant: CGFloat = 0) -> NSLayoutConstraint {
+        return topAnchor.constraint(equalTo: view.topAnchor, constant: constant)
+    }
+    
+    func bottom(in view: UIView, _ constant: CGFloat = 0) -> NSLayoutConstraint {
+        return bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: constant)
+    }
+    
+    func position(_ layoutPosition: LayoutPosition, in view: UIView, _ constant: CGFloat = 0) -> NSLayoutConstraint {
+        switch layoutPosition {
+        case .leading:
+            return leadingAnchor.constraint(equalTo: view.trailingAnchor, constant: constant)
+        case .trailing:
+            return trailingAnchor.constraint(equalTo: view.leadingAnchor, constant: constant)
+        case .top:
+            return topAnchor.constraint(equalTo: view.bottomAnchor, constant: constant)
+        case .bottom:
+            return bottomAnchor.constraint(equalTo: view.topAnchor, constant: constant)
+        }
+    }
+    
+    func width(_ w: CGFloat) -> NSLayoutConstraint {
+        return widthAnchor.constraint(equalToConstant: w)
+    }
+    
+    func width(in view: UIView, ratio: CGFloat = 1) -> NSLayoutConstraint {
+        return widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: ratio)
+    }
+    
+    func height(_ h: CGFloat) -> NSLayoutConstraint {
+        return heightAnchor.constraint(equalToConstant: h)
+    }
+    
+    func height(in view: UIView, ratio: CGFloat = 1) -> NSLayoutConstraint {
+        return heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: ratio)
     }
     
 }
