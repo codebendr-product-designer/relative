cb72ff6c953e514aacfb8c2881f0a652ffd9c2a1
codebendr-product-designer <codebendr@gmail.comm>
2021-02-19 23:56:12 +0000

    added a unit testing target

--- Sources/Relative/Extensions/UIView.swift
+++ Sources/Relative/Extensions/UIView.swift
@@ -42,16 +42,12 @@ public extension UIView {
         self.layer.addSublayer(bottomBorder)
         layoutIfNeeded()
     }
-    
-    
-    
+
     class func fromNib<T: UIView>() -> T {
         return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
     }
     
 }
-
-
 
 // MARK: Layout
 public enum LayoutPosition {
