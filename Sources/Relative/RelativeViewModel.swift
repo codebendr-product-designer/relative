b94cb0ef5e32421cdde28e4e5fe1a83feb371b8c
codebendr-product-designer <codebendr@gmail.comm>
2021-02-22 06:37:40 +0000

    added a `RelativeViewModel` to control stateless and stateful views

---
+++ Sources/Relative/RelativeViewModel.swift
@@ -0,0 +1,14 @@
+
+protocol RelativeViewModel: Relative {
+    var view: RelativeViewController<ChildView>? { get set }
+    var coordinator: MainCoordinator? { get set }
+    var childView: ChildView? { get set }
+    init(_ view: RelativeViewController<ChildView>, _ coordinator: MainCoordinator)
+    func present()
+}
+
+extension RelativeViewModel {
+    func present() {
+        view?.coordinator?.present(view: view!)
+    }
+}
