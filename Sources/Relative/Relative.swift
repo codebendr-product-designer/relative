82446680e1c6006e5c42071f8101aa3b8658dfce
codebendr-product-designer <codebendr@gmail.comm>
2021-02-22 06:21:04 +0000

    add a Relative protocol abstraction to facilitate the creation of views

--- Sources/Relative/Relative.swift
+++ Sources/Relative/Relative.swift
@@ -2,3 +2,14 @@
 public class Relative {
     
 }
+
+protocol Relative {
+    associatedtype ChildView: RelativeView
+    var coordinator: MainCoordinator? { get set }
+    var view: RelativeViewController<ChildView>? { get set }
+    var childView: ChildView? { get set }
+}
+
+
+
+
