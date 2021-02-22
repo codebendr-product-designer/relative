82a51c790959417317f2dbded363560764b1a06b
codebendr-product-designer <codebendr@gmail.comm>
2021-02-22 06:38:57 +0000

    minor file changes

---
+++ Sources/Relative/RelativeViewType.swift
@@ -0,0 +1,15 @@
+
+//add associated values to pass view information
+public enum RelativeViewType {
+    
+    //Account
+    case register
+    case login
+    case otp
+    case noOtpCode
+    
+    //Home
+    case main
+    case settings
+    case none
+}
